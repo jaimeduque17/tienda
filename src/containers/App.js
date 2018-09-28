34c6929301b1b5184cb178c3a70d938285e9b372
Silvia Londoño <silvialondono@MacBook-Pro-de-Silvia.local>
2018-09-27 23:16:51 -0500

    some components are created

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
+import Shelf from '../components/Shelf';
 import './App.css';
+
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            products: []
-        }
-    }
-    componentDidMount() {
-        fetch('//localhost:8001/api/products')
-            .then(res => res.json())
-            .then((json) => {
-                const products = json.products;
-                this.setState({ products });
-            })
-    }
+    
     render() {
-        console.log(this.state.products);
+
         return (
             <div className="App">
+            <Shelf />
             </div>
         );
     }
 }
+
 export default App;
